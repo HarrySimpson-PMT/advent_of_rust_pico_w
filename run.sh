dfa8c91b06000a8252049b1abfd06b56a80e636a
Harry Simpson <harrymsimpson@gmail.com>
2024-12-07 15:32:15 -0600

    created local run script

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+trap 'exit' INT
+
+# Run the code on the board
+probe-rs reset --chip rp2040 --protocol swd
+
+sleep 2
+
+probe-rs run --chip RP2040 --allow-erase-all ./target/thumbv6m-none-eabi/debug/wifi_tcp_server 
+# probe-rs run --chip RP2040 target/thumbv6m-none-eabi/debug/wifi_tcp_server -- moved from the build folder to the deployment folder.
+# ./scripts/run.sh -This is to run
+# chmod +x myscript.sh -This is to make executable
